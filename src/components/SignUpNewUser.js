e98dc9e64e8d443e2b4237a938f75d3bf73ee958
VictoriaStudios <88744359+VictoriaStudios@users.noreply.github.com>
2021-11-16 10:21:55 -0500

    Profile can now be created with or without picture, error handling improved, user can keep trying to find a suitable profile picture now

--- src/components/SignUpNewUser.js
+++ src/components/SignUpNewUser.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react"
 import useStyles from "./styles"
-import { Button, TextField } from "@material-ui/core"
+import { Button, TextField, Typography } from "@material-ui/core"
 import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import { db } from "../utils/Firebase";
 import { set, push, ref, child, get } from "firebase/database"
@@ -18,6 +18,8 @@ const SignUpNewUser = ({ loggedIn, onCloseHandler }) => {
     const [formVisible, setFormVisible] = useState(true)
     const [enteredWrong, setEnteredWrong] = useState(false)
     const [errorMessage, setErrorMessage] = useState("")
+    const [wrongFile, setWrongFile] = useState(false)
+    const [uploadErrorMessage, setUploadErrorMessage] = useState("")
     const [image, setImage] = useState("")
 
     const getProfileLink = (imageName, uid) => {
@@ -31,7 +33,7 @@ const SignUpNewUser = ({ loggedIn, onCloseHandler }) => {
 
     const handleSubmit = (e) => {
         e.preventDefault()
-        createNewUser(auth, email, password)
+        createNewUser()
     }
 
     const makeFormVisible = (bool) => {
@@ -39,43 +41,60 @@ const SignUpNewUser = ({ loggedIn, onCloseHandler }) => {
     }
 
 
-    function createNewUser(auth, email, password) {
+    function createNewUser() {
         console.log("Trying to create new user")
         if (password === passwordRepeat) {
-            createUserWithEmailAndPassword(auth, email, password)
-                .then((userCredential) => {
-                    var user = userCredential.user
-                    setEnteredWrong(false)
-                    createUserData(firstName, familyName, user.uid)
-                        .then(() => {
-                            if (image.size < 5 * 1024 * 1024 && image.type.match('image.*')) {
-                                saveImage(image, user.uid, getProfileLink)
-                            }
-                            else{
-                                console.log ("File is too large or not a picture")
-                            }
-                        })
+            if (image != "")
+            {
+                if (image.size < 5 * 1024 * 1024 && image.type.match('image.*')) {
+                    createNewUserData ()
+                }
+                else{
+                    setUploadErrorMessage ("Image larger than 5 MB or not an image at all")
+                    setWrongFile (true)
+                    setImage ("")
+                }
+            }
+            else {
+                createNewUserData ()
+            }
 
-                })
-                .catch((error) => {
-                    const errorCode = error.code;
-                    console.log(errorCode)
-                    if (errorCode === "auth/email-already-in-use") {
-                        setEnteredWrong(true)
-                        setErrorMessage("Email is already in use")
-                    }
-
-                    if (errorCode === "auth/weak-password") {
-                        setEnteredWrong(true)
-                        setErrorMessage("Password too weak")
-                    }
-                });
+            
         }
         else {
             setEnteredWrong(true)
             setErrorMessage("The passwords are not identical")
         }
 
+    }
+
+
+    function createNewUserData () {
+        createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            var user = userCredential.user
+            setEnteredWrong(false)
+            createUserData(firstName, familyName, user.uid)
+                .then(() => {
+                    if (wrongFile === false && image != "") {
+                        saveImage(image, user.uid, getProfileLink)
+                    }
+                })
+
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            console.log(errorCode)
+            if (errorCode === "auth/email-already-in-use") {
+                setEnteredWrong(true)
+                setErrorMessage("Email is already in use")
+            }
+
+            if (errorCode === "auth/weak-password") {
+                setEnteredWrong(true)
+                setErrorMessage("Password too weak")
+            }
+        });
     }
 
     useEffect(() => {
@@ -140,12 +159,15 @@ const SignUpNewUser = ({ loggedIn, onCloseHandler }) => {
                 />
                 <div />
                 {enteredWrong ? (<h6 style={{ color: "red", textAlign: "center" }}> {errorMessage} </h6>) : ""}
+               
                 <Button variant="contained" component="label">
                     Select File
-                    <input onChange={(e) => { setImage(e.target.files[0]) }}
+                    <input onChange={(e) => { setImage(e.target.files[0], setUploadErrorMessage (""), setWrongFile (false) )  }}
                         type="file"
                         hidden />
                 </Button>
+                {image != "" ? (<h6> {image.name} </h6>) : ("")}
+                {wrongFile ? (<h6 style={{ color: "red", textAlign: "center" }}> {uploadErrorMessage} </h6>) : ""}
                 <div className={classes.loginForm} style={{ textAlign: "center", height: "10vh", marginTop: "1rem", marginBottom: "1rem" }}>
                     <Button type="submit" color="default" >
                         Sign Up
